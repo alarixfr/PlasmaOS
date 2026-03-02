b396d363c89b21baede2f0adafb9abd4f8a629bd
Alaric Abyasa <alaricabyasa11@gmail.com>
2026-03-02 16:04:02 +0700

    Added taskbar and infobar

--- assets/js/system.js
+++ assets/js/system.js
@@ -1,8 +1,21 @@
+const lockscreenContainer = document.getElementById('lockscreen-container');
+const systemUI = document.getElementById('system-ui');
 const time = document.getElementById("lockscreen-time");
 const greet = document.getElementById("lockscreen-greet");
 const passInput = document.getElementById('lockscreen-input');
 const greetingList = ["Hello!", "Welcome Back!", "How's Today?"]
 const passcode = 'plasmaos';
+
+let unlocked = false;
+
+function unlock() {
+    setTimeout(() => {
+        lockscreenContainer.remove();
+        document.body.style.backdropFilter = 'none';
+        unlocked = true;
+        initUnlock();
+    }, 500);
+}
 
 function setTime() {
     const now = new Date();
@@ -24,6 +37,8 @@ function init() {
     setGreeting();
     const setTimeInterval = setInterval(setTime, 1000 * 60);
 
+    systemUI.style.display = 'none';
+
     passInput.addEventListener('input', () => {
         passInput.classList.remove('passcode-correct');
         passInput.classList.remove('passcode-error');
@@ -31,12 +46,17 @@ function init() {
             if (passInput.value === passcode) {
                 passInput.classList.add('passcode-correct');
                 console.log('Correct!');
+                unlock();
             } else {
                 passInput.classList.add('passcode-error');
                 console.log(`Wrong! Passcode: ${passcode}`);
             }
         }
     });
+}
+
+function initUnlock() {
+    systemUI.style.display = 'flex';
 }
 
 init();
